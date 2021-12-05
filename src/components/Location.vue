bfdb6daa61e147b83c6e6c89c869a7af71a09665
kyileeyu <kyileeeyu@gmail.com>
2021-12-05 18:31:41 +0900

    Add: kakao map api

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -30,39 +30,88 @@
 <script>
 import axios from 'axios';
 
+// var positions = [
+//   {
+ 
+//     latlng: new kakao.maps.LatLng(37.562632898194835, 126.9454282268269),
+//   },
+//   {
 
-var mapContainer = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var mapOption = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-	level: 2 //지도의 레벨(확대, 축소 정도)
-};
-
-
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+//     latlng: new kakao.maps.LatLng(37.56195884514403, 126.94922601468826),
+//   }
+// ]
   
-// 마커를 표시할 위치입니다 
-var position =  new kakao.maps.LatLng(33.450701, 126.570667);
 
-// 마커를 생성합니다
-var marker = new kakao.maps.Marker({
-  position: position,
-  clickable: true // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-});
-marker.setMap(map);
-var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+//   var imageSrc = require('@/assets/marker.png'), // 마커이미지의 주소입니다
+//     imageSize = new kakao.maps.Size(24, 35), // 마커이미지의 크기입니다
+//     imageOption = { offset: new kakao.maps.Point(20, 35) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-// 인포윈도우를 생성합니다
-var infowindow = new kakao.maps.InfoWindow({
-    content : iwContent,
-    removable : iwRemoveable
-});
+// // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+// var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
-// 마커에 클릭이벤트를 등록합니다
-kakao.maps.event.addListener(marker, 'click', function() {
-      // 마커 위에 인포윈도우를 표시합니다
-      infowindow.open(map, marker);  
-});
+// // 마커를 생성합니다
+//   positions.forEach(function(pos) {
+//     var marker = new kakao.maps.Marker({
+//       map: map, // 마커를 표시할 지도
+//       position: pos.latlng, // 마커의 위치
+//       image: markerImage,
+//     });
+// //   },
+
+// // // 마커를 표시할 위치입니다 
+// // var positions =   [
+// //     {
+// //         content: '<div>카카오</div>', 
+// //         latlng: new window.kakao.maps.LatLng(33.450705, 126.570677)
+// //     },
+// //     {
+// //         content: '<div>생태연못</div>', 
+// //         latlng: new window.kakao.maps.LatLng(33.450936, 126.569477)
+// //     },
+// //     {
+// //         content: '<div>텃밭</div>', 
+// //         latlng: new window.kakao.maps.LatLng(33.450879, 126.569940)
+// //     },
+// //     {
+// //         content: '<div>근린공원</div>',
+// //         latlng: new window.kakao.maps.LatLng(33.451393, 126.570738)
+// //     }
+// // ];
+
+
+// // for (var i = 0; i < positions.length; i ++) {
+// //     // 마커를 생성합니다
+// //     var marker = new window.kakao.maps.Marker({
+// //         map: map, // 마커를 표시할 지도
+// //         position: positions[i].latlng // 마커의 위치
+// //     });
+
+// //     // 마커에 표시할 인포윈도우를 생성합니다 
+// //     var infowindow = new window.kakao.maps.InfoWindow({
+// //         content: positions[i].content // 인포윈도우에 표시할 내용
+// //     });
+
+// //     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+// //     // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+// //     // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+// //     window.kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+// //     window.kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+// // }
+
+// // // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+// // function makeOverListener(map, marker, infowindow) {
+// //     return function() {
+// //         infowindow.open(map, marker);
+//     };
+// }
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+// function makeOutListener(infowindow) {
+//     return function() {
+//         infowindow.close();
+//     };
+// }
+
 
 
 
@@ -91,12 +140,36 @@ export default {
             }
             );
         },
+        initMap() {
+        var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+            mapOption = {
+            center: new kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
+            level: 3, // 지도의 확대 레벨
+            };
+        var map = new kakao.maps.Map(mapContainer, mapOption);
+        const markerPosition = new kakao.maps.LatLng(35.19656853772262, 129.0807270648317);
+
+        const marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+        marker.setMap(map)
+        }
 
     },
     mounted(){
-        this.getChimai()
+        if (window.kakao && window.kakao.maps) {
+            this.initMap();
+        } else {
+            const script = document.createElement('script');
+            /* global kakao */
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src =
+            'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=2844ff5ba6815df9b368effb5f45d7b9&libraries=clusterer';
+            document.head.appendChild(script);
+        }
+    },
 
-    }
+    
 }
 </script>
 
