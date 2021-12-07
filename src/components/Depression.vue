458d4f88cc34b04215be01a5937fb7a47c40cab2
kyileeyu <kyileeeyu@gmail.com>
2021-12-08 04:39:20 +0900

    Add: 마커표시 추가

--- src/components/Depression.vue
+++ src/components/Depression.vue
@@ -1,6 +1,5 @@
 <template>
 <div class="result">
-    <!-- <img src="../assets/1.png" class="result-img" /> -->
     <h3 class="result-title">
         우울증 진단 및 치료 무료 프로그램 안내 입니다.
 
@@ -19,7 +18,7 @@
      <p class="result-desc red">
         ▷ 가까운 센터를 확인하세요
     </p>
-    <div id="map" class="map" style="width:340px;height:400px;">
+    <div id="map" class="map" style="width:340px;height:400px;" >
         
 
 
@@ -34,110 +33,32 @@
 <script>
 import axios from 'axios';
 
-// var positions = [
-//   {
- 
-//     latlng: new kakao.maps.LatLng(37.562632898194835, 126.9454282268269),
-//   },
-//   {
-
-//     latlng: new kakao.maps.LatLng(37.56195884514403, 126.94922601468826),
-//   }
-// ]
-  
-
-//   var imageSrc = require('@/assets/marker.png'), // 마커이미지의 주소입니다
-//     imageSize = new kakao.maps.Size(24, 35), // 마커이미지의 크기입니다
-//     imageOption = { offset: new kakao.maps.Point(20, 35) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-// // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-// var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-// // 마커를 생성합니다
-//   positions.forEach(function(pos) {
-//     var marker = new kakao.maps.Marker({
-//       map: map, // 마커를 표시할 지도
-//       position: pos.latlng, // 마커의 위치
-//       image: markerImage,
-//     });
-// //   },
-
-// // // 마커를 표시할 위치입니다 
-// // var positions =   [
-// //     {
-// //         content: '<div>카카오</div>', 
-// //         latlng: new window.kakao.maps.LatLng(33.450705, 126.570677)
-// //     },
-// //     {
-// //         content: '<div>생태연못</div>', 
-// //         latlng: new window.kakao.maps.LatLng(33.450936, 126.569477)
-// //     },
-// //     {
-// //         content: '<div>텃밭</div>', 
-// //         latlng: new window.kakao.maps.LatLng(33.450879, 126.569940)
-// //     },
-// //     {
-// //         content: '<div>근린공원</div>',
-// //         latlng: new window.kakao.maps.LatLng(33.451393, 126.570738)
-// //     }
-// // ];
-
-
-// // for (var i = 0; i < positions.length; i ++) {
-// //     // 마커를 생성합니다
-// //     var marker = new window.kakao.maps.Marker({
-// //         map: map, // 마커를 표시할 지도
-// //         position: positions[i].latlng // 마커의 위치
-// //     });
-
-// //     // 마커에 표시할 인포윈도우를 생성합니다 
-// //     var infowindow = new window.kakao.maps.InfoWindow({
-// //         content: positions[i].content // 인포윈도우에 표시할 내용
-// //     });
-
-// //     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-// //     // 이벤트 리스너로는 클로저를 만들어 등록합니다 
-// //     // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-// //     window.kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-// //     window.kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-// // }
-
-// // // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-// // function makeOverListener(map, marker, infowindow) {
-// //     return function() {
-// //         infowindow.open(map, marker);
-//     };
-// }
-
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
-// function makeOutListener(infowindow) {
-//     return function() {
-//         infowindow.close();
-//     };
-// }
-
-
-
-
 
 export default {
-  name: 'Result',
+    name: 'Result',
   data(){
-    return {
+      return {
     //   showIntro: true,
     si:['하남시'],
+    chimai:[],
+    depression:[],
+    noSmoke:[],
+    check:[],
+    baby:[],
+    cancer:[],
 
     }
   },
   components: {
-   
-  },
+      
+      },
     methods:{
         getChimai(){
-            axios.get('https://drkjhyuck.pythonanywhere.com/dementiactr/?city_name=하남시')
+            axios.get('https://drkjhyuck.pythonanywhere.com/dementiactr/?city_name=부천시')
             .then(res =>{
-                console.log(res);
                 console.log('치매 성공!');
+                console.log(res);
+                this.chimai = res.data;
                 })
             .catch((error) => {
                 console.error('치매 에러는'+error);
@@ -148,89 +69,136 @@ export default {
             axios.get('https://drkjhyuck.pythonanywhere.com/nosmokectr/?high_city=경기도')
             .then(res =>{
                 console.log(res);
-                console.log('금연센터!');
+                this.smoke = res.data.result;
+                console.log('받아온값'+this.smoke);
+
                 })
             .catch((error) => {
                 console.error('금연 에러는'+error);
                 }
             );
         },
-        getChimaiii(){
-            axios.get('https://drkjhyuck.pythonanywhere.com/bluectr/?city_name=용인시')
-            .then(res =>{
-                console.log(res);
-                console.log('우울증 성공!');
-                })
-            .catch((error) => {
-                console.error('우울증 에러는'+error);
-                }
-            );
-        },
-        getChimaiiii(){
-            axios.get('https://drkjhyuck.pythonanywhere.com/normalchk/?age=40&format=json&gender=Female')
-            .then(res =>{
-                console.log(res);
-                console.log('건강검진 성공!');
-                })
-            .catch((error) => {
-                console.error('건강검진 에러는'+error);
-                }
-            );
-        },
-        getChimaiiiii(){
-            axios.get('https://drkjhyuck.pythonanywhere.com/childchk/?childage=66&format=json')
-            .then(res =>{
-                console.log(res);
-                console.log('영유아 성공!');
-                })
-            .catch((error) => {
-                console.error('영유아 에러는'+error);
-                }
-            );
-        },
-        getChimaiiiiii(){
-            axios.get('https://drkjhyuck.pythonanywhere.com/cancerchk/?gender=Female&format=json&age=46')
-            .then(res =>{
-                console.log(res);
-                console.log('암정보 성공!');
-                })
-            .catch((error) => {
-                console.error('암정보 에러는'+error);
-                }
-            );
-        },
+        
+        // getChimaiii(){
+        //     axios.get('https://drkjhyuck.pythonanywhere.com/bluectr/?city_name=부천시')
+        //     .then(res =>{
+        //         console.log(res);
+        //         console.log('우울증 성공!');
+        //         })
+        //     .catch((error) => {
+        //         console.error('우울증 에러는'+error);
+        //         }
+        //     );
+        // },
+        // getChimaiiii(){
+        //     axios.get('https://drkjhyuck.pythonanywhere.com/normalchk/?age=40&format=json&gender=Female')
+        //     .then(res =>{
+        //         console.log(res);
+        //         console.log('건강검진 성공!');
+        //         })
+        //     .catch((error) => {
+        //         console.error('건강검진 에러는'+error);
+        //         }
+        //     );
+        // },
+        // getChimaiiiii(){
+        //     axios.get('https://drkjhyuck.pythonanywhere.com/childchk/?childage=66&format=json')
+        //     .then(res =>{
+        //         console.log(res);
+        //         console.log('영유아 성공!');
+        //         })
+        //     .catch((error) => {
+        //         console.error('영유아 에러는'+error);
+        //         }
+        //     );
+        // },
+        // getChimaiiiiii(){
+        //     axios.get('https://drkjhyuck.pythonanywhere.com/cancerchk/?gender=Female&format=json&age=46')
+        //     .then(res =>{
+        //         console.log(res);
+        //         console.log('암정보 성공!');
+        //         })
+        //     .catch((error) => {
+        //         console.error('암정보 에러는'+error);
+        //         }
+        //     );
+        // },
 
         
         initMap() {
-        var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+            var mapContainer = document.getElementById('map'), // 지도를 표시할 div
             mapOption = {
-            center: new kakao.maps.LatLng(37.52487287539575, 126.90942458792783), // 지도의 중심좌표
-            level: 3, // 지도의 확대 레벨
+            center: new kakao.maps.LatLng(37.4706038241,126.8170617140), // 지도의 중심좌표
+            level: 8, // 지도의 확대 레벨
             };
         var map = new kakao.maps.Map(mapContainer, mapOption);
-        const markerPosition = new kakao.maps.LatLng(35.19656853772262, 129.0807270648317);
+        
+        var positions = [
+            {
+                content: '<div>경기도 부천시 양지로 134<br>032-625-9871</div>', 
+                latlng: new kakao.maps.LatLng(37.4706038241, 126.817061714)
+            },
+            {
+                content: '<div>경기도 부천시 옥산로10번길 16<br>032-625-4423</div>', 
+                latlng: new kakao.maps.LatLng(37.49426645, 126.7809633628)
+            },
+            {
+                content: '<div>"경기도 부천시 성오로 172"<br>032-625-9881</div>', 
+                latlng: new kakao.maps.LatLng(37.5283469861, 126.7960281955)
+            },
+            {
+                content: '<div>근린공원</div>',
+                latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+            }
+        ];
 
-        const marker = new kakao.maps.Marker({
-            position: markerPosition
-        });
-        marker.setMap(map)
+        for (var i = 0; i < 4; i ++) {
+            // 마커를 생성합니다this.chimai[0].demen_logt
+            // var lating = new window.kakao.maps.LatLng(this.chimai[0].demen_lat,this.chimai[0].demen_logt)
+            // var lating = new window.kakao.maps.LatLng( 37.4706038241,126.8170617140)
+            var marker = new window.kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커의 위치,
+            });
+            var infowindow = new kakao.maps.InfoWindow({
+                content: positions[i].content // 인포윈도우에 표시할 내용
+            });
+            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
         }
+        function makeOverListener(map, marker, infowindow) {
+            return function() {
+                infowindow.open(map, marker);
+            };
+        }
+        
+        // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+        function makeOutListener(infowindow) {
+            return function() {
+                infowindow.close();
+            };
+        }
+        }
+        
+
+
+
 
     },
     mounted(){
         this.getChimai();
         this.getChimaii();
-        this.getChimaiii();
-        this.getChimaiiii();
-        this.getChimaiiiii();
-        this.getChimaiiiiii();
+        // this.getChimaiii();
+        // this.getChimaiiii();
+        // this.getChimaiiiii();
+        // this.getChimaiiiiii();
         if (window.kakao && window.kakao.maps) {
             this.initMap();
         } else {
             const script = document.createElement('script');
             script.onload = () => kakao.maps.load(this.initMap);
             script.src =
-            'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=2844ff5ba6815df9b368effb5f45d7b9&libraries=clusterer';
+            'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=14cc464a948df948e07660a20c3ae220';
             document.head.appendChild(script);
         }
     },
